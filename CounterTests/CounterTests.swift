1e77ad5435f432b4e1daf0c8d2d346fbc9585515
limonova <limonlnz@icloud.com>
2024-11-08 14:18:21 +0300

    Initial Commit

---
+++ CounterTests/CounterTests.swift
@@ -0,0 +1,17 @@
+//
+//  CounterTests.swift
+//  CounterTests
+//
+//  Created by natalia.limonova on 08.11.2024.
+//
+
+import Testing
+@testable import Counter
+
+struct CounterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
