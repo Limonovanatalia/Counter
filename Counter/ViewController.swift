a694347f8f8c709edf76ab14893c8886579c7f89
limonova <limonlnz@icloud.com>
2024-11-11 14:33:11 +0300

    Лёгкая задачка

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var counter = 0
+    
+    @IBOutlet weak var textChangeLabel: UITextField!
+    @IBOutlet weak var changeButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        change()
+        textChangeLabel.tintColor = .lightGray
+        textChangeLabel.textColor = .blue
+        }
+    func change () {
+        textChangeLabel.text = "ЗНАЧЕНИЕ СЧЁТЧИКА: \(counter)"
+       }
+    @IBAction func buttonDidTap(_ sender: Any) {
+        print("Нажатие")
+        counter += 1
+        change()
     }
-
-
 }
+    
+
 
